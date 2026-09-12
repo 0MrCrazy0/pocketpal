14b824264f6fad1f34af3f91f1c2ddfab3a0606b
MRCRAZY <102135865+0MrCrazy0@users.noreply.github.com>
2026-09-12 18:30:29 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-/* PocketPal Service Worker v2.35 */
-const CACHE = 'pocketpal-v235';
+/* PocketPal Service Worker v2.36 */
+const CACHE = 'pocketpal-v236';
 const ASSETS = [
   './', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon.svg'
 ];
